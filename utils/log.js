a974970dfe7091f290308b904cc091cf56d90057
hiukaidu <131503089+hiukaidu@users.noreply.github.com>
2023-07-30 11:47:21 +0700

    Update log.js
    Uppp

--- utils/log.js
+++ utils/log.js
@@ -1,29 +1,36 @@
 const chalk = require('chalk');
-
+function randomColor() {
+    var color = "";
+    for (var i = 0; i < 3; i++) {
+        var sub = Math.floor(Math.random() * 256).toString(16);
+        color += (sub.length == 1 ? "0" + sub : sub);
+    } 
+   return "#" + color;
+};
 module.exports = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('[ ❕ ] » ') + data);
+				console.log(chalk.bold.hex("#ff0000").bold('◁  🦄Hết-Cứu🦄  ▷ ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('[ ❕ ] » ') + data);
+			console.log(chalk.bold.hex("#ff0000").bold('◁  🦄Chịu-Thua🦄  ▷ ') + data);
 			break;
 		default:
-			console.log(chalk.magenta(`${option} » `) + data);
+				console.log(chalk.bold.hex(randomColor()).bold(`${option} » `) + data);
 			break;
 	}
 }
 
 module.exports.loader = (data, option) => {
 	switch (option) {
-		case "warn":
-			console.log(chalk.yellow('[ LOADER ] » ') + data);
+    case "warn":
+			console.log(chalk.bold.hex(randomColor()).bold("─🌸🐧𝑻𝒓𝒖𝒏𝒈 𝑯𝒊𝒆̂́𝒖🐧🌸─ ▷ ") + chalk.bold.hex("#8B8878").bold(data))
 			break;
 		case "error":
-			console.log(chalk.red('[ LOADER ] » ') + data);
+		console.log(chalk.bold.hex(randomColor()).bold("─🌸🐧𝑻𝒓𝒖𝒏𝒈 𝑯𝒊𝒆̂́𝒖🐧🌸─ ▷ ") + data);
 			break;
-		default:
-			console.log(chalk.green(`[ LOADER ] » `) + data);
+    default:
+			console.log(chalk.bold.hex(randomColor()).bold("─🌸🐧𝑻𝒓𝒖𝒏𝒈 𝑯𝒊𝒆̂́𝒖🐧🌸─ ▷ ") + chalk.bold.hex(randomColor()).bold(data));
 			break;
 	}
 }
